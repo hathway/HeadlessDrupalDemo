fdc493c68a567b6a41699f663c4fea79ef628ea3
Mike Maxwell <mike@wearehathway.com>
2015-07-29 19:01:18 -0700

    More content structure changes

--- app/content/content.js
+++ app/content/content.js
@@ -16,5 +16,13 @@ angular.module('myApp.content', ['ngRoute'])
         drupal.node_load(1).then(function (node) {
             $scope.content = node;
             $scope.body = node.body.und[0].safe_value;
+
+            drupal.file_load(node.field_hero_image.und[0].fid).then(function(file) {
+                $scope.heroImage = {
+                    background: 'url(' + file.uri_full + ')'
+                };
+                $scope.heroImageUrl = file.uri_full;
+            });
+
         });
     }]);
